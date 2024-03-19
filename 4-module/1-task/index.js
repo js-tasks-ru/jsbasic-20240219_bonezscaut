73cb80cd3c97dc57521992206f3d5472f4d9448c
Stanislav Sherstiukov <bonezscaut@gmail.com>
2024-03-19 21:55:48 +0400

    M4_alltasks

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,28 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement('ul');
+
+  friends.forEach(friend => {
+    let li = document.createElement('li');
+    li.textContent = `${friend.firstName} ${friend.lastName}`;
+    ul.appendChild(li);
+  });
+
+  return ul;
 }
+
+let friends = [
+    {
+        firstName: 'Artsiom',
+        lastName: 'Mezin'
+    },
+    {
+        firstName: 'Ilia',
+        lastName: 'Kantor'
+    },
+    {
+        firstName: 'Christopher',
+        lastName: 'Michael'
+    }
+];
+
+document.body.appendChild(createFriendsList(friends));
--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,9 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  for (let i = 0; i < table.rows.length; i++) {
+    let cell = table.rows[i].cells[i];
+    cell.style.backgroundColor = 'red';
+  }
 }
+
+let table = document.getElementById('table');
+makeDiagonalRed(table);
